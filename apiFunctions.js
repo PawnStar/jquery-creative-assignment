c7fd8027810b4991c51821838f7ae1b5c278093f
Cole Erickson <pawnstar4@gmail.com>
2018-10-03 22:15:13 -0600

    Implemented

--- apiFunctions.js
+++ apiFunctions.js
@@ -10,49 +10,75 @@ const LOCATION_API_KEY = 'b920938457a097523b487e967894099a';
  * all that is in a different file.
  */
 
+ // Helper function that does fetch -> json
 const getJSON = async url=>{
   const response = await fetch(url);
   return await response.json();
 }
 
 const getUserLocation = async ()=>{
-  const data = await getJSON(`http://api.ipstack.com/check?access_key=${LOCATION_API_KEY}`);
+  const location = await getJSON(`http://api.ipstack.com/check?access_key=${LOCATION_API_KEY}`);
 
-  return data.city;
+  return [
+    ...location.city.split(' '),
+    ...location.region_name.split(' ')
+  ]
 }
 
-const getNewspapers = async city=>{
-  const data = await getJSON(`https://chroniclingamerica.loc.gov/suggest/titles/?q=${city}`);
+// Get all the papers published in location
+const getPapers = async location=>{
+  const data = await getJSON(`https://chroniclingamerica.loc.gov/search/titles/results/?terms=${location.join('+')}&format=json`);
 
-  let papers = data[1].map((name, index)=>({
-    name,
-    id: data[2][index],
-    url: data[3][index]
-  }));
-
-  papers = await Promise.all(papers.map(async paper=>{
-    return await getDataForNewspaper(paper.id);
-  }))
+  // Make sure it was in that city, not just having the city in the title
+  let papers = data.items.filter(paper=>{
+    let pub = paper.place_of_publication.toLowerCase();
+    for(let component of location)
+      if(!pub.includes(component.toLowerCase()))
+        return false;
+    return true;
+  })
 
   return papers;
 }
 
-getDataForNewspaper = async newspaperID=>{
-  return await getJSON(`https://chroniclingamerica.loc.gov/lccn/${newspaperID}.json`);
+// Transform array of papers into promises retrieving all the issues of that paper available
+// wait on those promises and return array of populated papers
+const populateIssues = papers=>Promise.all(papers.map(paper=>getJSON(paper.url.replace('http://', 'https://'))))
+
+// Filter for papers with issues on current day
+const filterToday = async papers=>{
+  // Filter issues that happened on this day
+  let date = '-' + ((new Date()).getMonth() + 1 + '').padStart(2, '0') + '-' + ((new Date()).getDate() + 2 + '').padStart(2, '0');
+  console.log('filtering by ' + date);
+  papers.map(paper=>{
+    paper.issues = paper.issues.filter(issue=>issue.date_issued.includes(date))
+    return paper
+  })
+
+  return papers.filter(paper=>paper.issues.length > 0)
 }
 
-const getIssuesForNewspaper = async newspaperID=>{
-  const data = await getDataForNewspaper(newspaperID);
+// Randomly select an issue from the papers/issues passed in
+const getRandomIssue = async (papers)=>{
+  // Randomly select newspaper
+  let newspaper = papers[Math.floor(Math.random()*papers.length)]
 
-  return data.issues;
+  // Randomly select issue
+  let issue = newspaper.issues[Math.floor(Math.random()*newspaper.issues.length)]
+
+  return issue;
 }
 
 const getIssueFrontPage = async issue=>{
-  const issueData = await getJSON(issue.url);
+  const issueData = await getJSON(issue.url.replace('http://', 'https://'));
   const pageURL = issueData.pages[0].url;
-  const pageData = await getJSON(issueData.pages[0].url);
-  pageData.images = {
-    preview: pageURL.replace('.json', '/image_600x800_from_0,0_to_7468,10468.jpg'),
-    large: pageURL.replace('.json', '/image_1920x1080_from_0,0_to_7468,10468.jpg')
-  }
+  const pageData = await getJSON(issueData.pages[0].url.replace('http://', 'https://'));
+
+  // Make reverse-engineered image links
+  pageData.image = pageURL.replace('.json', '/image_600x800_from_0,0_to_7468,10468.jpg');
+
+  // Make html page link
+  pageData.htmlPage = pageURL.replace('.json', '');
+
+  return pageData;
 }
